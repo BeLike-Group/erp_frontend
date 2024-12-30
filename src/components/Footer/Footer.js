515d0e39ddbbc8f8bc265b70ff179d6446848973
Bakar002 <abubakaramjad78600@gmail.com>
2024-12-30 16:31:03 +0500

    adding

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -57,23 +57,7 @@ const Footer = () => {
 
         {/* Join Section */}
         <div>
-          <div className="flex flex-col items-center sm:flex-row sm:items-center ml-4">
-            <div className="text-center text-white bg-bg-inherit m-2 rounded-xl pr-12 border-2 bg-gray-900 p-3 border-gray-900">
-              <h1 className="text-1xl font-bold">JOIN OUR INVESTOR TODAY</h1>
-              <p className="mt-2 text-[0.95rem] tracking-wide">
-                Belike Group services involve SEO, social media PPC
-                <br />
-                advertising content marketing, and email
-                <br />
-                campaigns and email campaigns..
-              </p>
-              <button className="bg-green-500 px-4 py-2 rounded-md mt-4 hover:bg-green-300">
-                <a href="/">
-                  <p className="text-blue-900 font-semibold">Join Now</p>
-                </a>
-              </button>
-            </div>
-          </div>
+          {/*  */}
           {/* Social Media Icons */}
           <div className="flex justify-center mt-4">
             <a href="https://facebook.com" className="mx-2">
